fae0d6dc48b696ccb308e51b5d40f3fcd5e2d04b
João da Silva Campos <141191800+JoaoCamposDev@users.noreply.github.com>
2024-04-28 21:11:30 -0300

    Add files via upload

---
+++ src/Pages/contatos/Contato.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPhoneVolume, faTrashAlt, faUser } from "@fortawesome/free-solid-svg-icons"
+import './contatos.css'
+
+function Contatos(props) {
+    return (
+        <div className="mx-2">
+            <div className="container componente-contato my-4">
+                <div className="row">
+                    <div className="col p-2">
+                        <h4>
+                            <FontAwesomeIcon icon={faUser} className="me-3"></FontAwesomeIcon>
+                            {props.nome}
+                        </h4>
+                    </div>
+                    <div className="col p-2">
+                        <h4>
+                            <FontAwesomeIcon icon={faPhoneVolume} className="me-3"></FontAwesomeIcon>
+                            {props.telefone}
+                        </h4>
+                    </div>
+                    <div className="col p-2 text-end">
+                        <h4>
+                            <FontAwesomeIcon
+                                icon={faTrashAlt}
+                                className="me-3"
+                                onClick={() => { props.remover(props.id) }}></FontAwesomeIcon>
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default Contatos;
